cbccbe158b3719d59c49c31eede89c13ba3aebc2
Himanshu Sharma <himanshamra@gmail.com>
2018-12-07 14:54:47 -0600

    Updated Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Desk Nibbles Challenge
+
+run `make install` to install requests module
+
+run `make run` to execute program
+
+`out.txt` contains output
--- desknibbles.py
+++ desknibbles.py
@@ -1,26 +1,22 @@
 import json
 import requests
 
-
 currentStock = requests.get("https://ca.desknibbles.com/products.json?limit=250")
 snackerListResp = requests.get("https://s3.amazonaws.com/misc-file-snack/MOCK_SNACKER_DATA.json")
 
 stockJsonData = currentStock.json()["products"]
 snackerListJson = snackerListResp.json()
 
-productStock={}
+productStock = {}
 for cont in stockJsonData:
-    if cont["title"] not in productStock: productStock[cont["title"]]=cont
+  if cont["title"] not in productStock: productStock[cont["title"]] = cont
 
-snacksStocked=[]
-snackerEmail=[]
-totalSnackerPrice=0
+snacksStocked = []
+snackerEmail = []
+totalSnackerPrice = 0
 for snacker in snackerListJson:
-    if snacker["fave_snack"] in productStock:
-        print("{}:{}".format(snacker["email"],snacker["fave_snack"]))
-        totalSnackerPrice+=float(productStock[snacker["fave_snack"]]["variants"][0]["price"])
-
-print(totalSnackerPrice)
-
-
-
+  if snacker["fave_snack"] in productStock:
+    print("{}: {}".format(snacker["email"], snacker["fave_snack"]))
+    totalSnackerPrice += float(productStock[snacker["fave_snack"]]["variants"][0]["price"])
+print()
+print("Total Price: {}".format(totalSnackerPrice))
